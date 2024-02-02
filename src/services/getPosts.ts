22815310877f0c8b20cdd28d6ce2771ce8c30925
Stormlig <114263838+Stormlig@users.noreply.github.com>
2024-02-02 19:57:20 -0300

    add getPosts.ts

---
+++ src/services/getPosts.ts
@@ -0,0 +1,53 @@
+import * as prismic from '@prismicio/client'
+import { RTImageNode, RTParagraphNode, asHTML } from '@prismicio/client'
+import { createClient } from '../prismicio'
+import { PostDocument } from '../../prismicio-types'
+
+interface GetPostsParams {
+  page: number
+  queryFilter?: string
+}
+
+export async function getPosts(
+  params: GetPostsParams = {
+    page: 1,
+    queryFilter: '',
+  },
+) {
+  const { page, queryFilter } = params
+  const client = createClient()
+
+  const { results, total_pages } = await client.get({
+    fetch: 'posts',
+    pageSize: 10,
+    page,
+    orderings: {
+      field: 'document.first_publication_date',
+      direction: 'desc',
+    },
+    filters: queryFilter
+      ? [prismic.filter.fulltext('document', queryFilter)]
+      : undefined,
+  })
+
+  const posts = (results as PostDocument[]).map(postMapper)
+
+  return { posts, totalPages: total_pages }
+}
+
+function postMapper(post: PostDocument) {
+  const assert = post.data.content.find(
+    (content) => content.type === 'paragraph' && content.text !== '',
+  ) as RTParagraphNode | undefined
+
+  const firstImage = post.data.content.find(
+    (content) => content.type === 'image',
+  ) as RTImageNode | undefined
+
+  return {
+    ...post,
+    contentAsHtml: asHTML(post.data.content),
+    firstImage,
+    assert,
+  }
+}
